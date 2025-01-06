83b31135515dce5853818937ed6d73cd11c4c407
Vitaliy Vynnyk <25266258+dedVitalik@users.noreply.github.com>
2025-01-06 14:25:09 +0100

    configure prettier

--- src/components/Documents/Documents.tsx
+++ src/components/Documents/Documents.tsx
@@ -1,6 +1,6 @@
-import {Roboto, Source_Sans_3} from 'next/font/google';
+import { Roboto, Source_Sans_3 } from 'next/font/google';
 import Image from 'next/image';
-import {useTranslations} from 'next-intl';
+import { useTranslations } from 'next-intl';
 
 const roboto = Roboto({
   weight: ['500', '700'],
@@ -18,32 +18,32 @@ export function Documents() {
   return (
     <section
       className={` bg-cover bg-fixed bg-center`}
-      style={{backgroundImage: 'url("/images/documents/doc-bg.jpg")'}}
+      style={{ backgroundImage: 'url("/images/documents/doc-bg.jpg")' }}
     >
-      <div className="size-full bg-white bg-opacity-90 pb-28 pt-24">
-        <div className="wrapper">
-          <div className="mb-10 text-center leading-6 text-zinc-800">
+      <div className='size-full bg-white bg-opacity-90 pb-28 pt-24'>
+        <div className='wrapper'>
+          <div className='mb-10 text-center leading-6 text-zinc-800'>
             <h2
               className={`m-0 font-sans text-4xl font-semibold uppercase ${roboto.className}`}
-              style={{lineHeight: '1.3'}}
+              style={{ lineHeight: '1.3' }}
             >
               {t('section_name')}
             </h2>
-            <h5 className="mx-auto my-0 max-w-xl font-sans text-xl font-light uppercase leading-normal tracking-wider text-sky-600">
+            <h5 className='mx-auto my-0 max-w-xl font-sans text-xl font-light uppercase leading-normal tracking-wider text-sky-600'>
               {t('section_desc')}
             </h5>
           </div>
-          <div className="flex flex-row flex-wrap justify-center md:flex-nowrap">
-            <div className="basis-full-full justify-items-end md:basis-1/3">
+          <div className='flex flex-row flex-wrap justify-center md:flex-nowrap'>
+            <div className='basis-full-full justify-items-end md:basis-1/3'>
               <Image
                 alt={t('section_desc')}
-                className="max-w-full text-right "
+                className='max-w-full text-right '
                 height={270}
-                src="/images/documents/doc.jpg"
+                src='/images/documents/doc.jpg'
                 width={270}
               />
             </div>
-            <div className="w-4/5 basis-full bg-none p-8 text-center md:basis-2/3 md:bg-white md:text-left xl:basis-1/3">
+            <div className='w-4/5 basis-full bg-none p-8 text-center md:basis-2/3 md:bg-white md:text-left xl:basis-1/3'>
               <p
                 className={`m-0 pb-2  text-base  leading-6 tracking-wide text-neutral-600 placeholder:font-medium ${sourceSans.className}`}
               >
